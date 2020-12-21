823d97b9738395f0d42ded837f9fd2c000172f43
Mark Umnus <merkrafter@gmail.com>
2020-12-21 16:57:18 +0100

    Add Direction enum

---
+++ include/libsanjego/rulesets.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace libsanjego {
+/*
+ * Directions a player can move a tower.
+ */
+enum class Direction {
+  NORTH,
+  EAST,
+  SOUTH,
+  WEST,
+};
+}  // namespace libsanjego
